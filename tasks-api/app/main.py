7a8df07830dab18a2d7ad6d5caf2d0f3fbdeb805
Kedarini <kedardamps@gmail.com>
2026-09-24 12:38:08 +0200

    Reformat import

--- tasks-api/app/main.py
+++ tasks-api/app/main.py
@@ -1,6 +1,7 @@
+from fastapi import FastAPI
+
 from app.api.routes import tasks
 from app.core.config import get_settings
-from fastapi import FastAPI
 
 settings = get_settings()
 app = FastAPI(title=settings.app_name, debug=settings.debug)
