e7e6e6faec66208e53b26973d7c4c96f52fdba2f
Kedarini <kedardamps@gmail.com>
2026-09-24 12:44:26 +0200

    Remove unnecessary type definition

--- tasks-api/app/api/deps.py
+++ tasks-api/app/api/deps.py
@@ -5,6 +5,6 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.core.database import AsyncSessionLocal
 
 
-async def get_db() -> AsyncGenerator[AsyncSession, None]:
+async def get_db() -> AsyncGenerator[AsyncSession]:
     async with AsyncSessionLocal() as session:
         yield session
